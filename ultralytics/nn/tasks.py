9e8b0b2334109ccc32b45fc1af57105c4642cebd
absit23 <113936268+absit23@users.noreply.github.com>
2025-12-16 02:12:02 +0300

    Add C3k2_LSK to the task list

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1580,6 +1580,7 @@ def parse_model(d, ch, verbose=True):
             C2,
             C2f,
             C3k2,
+            C3k2_LSK,
             RepNCSPELAN4,
             ELAN1,
             ADown,
